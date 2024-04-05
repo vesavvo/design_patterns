266d959ca5ba5af138731c61ba190085bf539fc9
vesavvo <vesavvo@gmail.com>
2024-04-05 11:02:16 +0300

    Add final to intrinsic state member variables

--- src/main/java/flyweight/birch_trees/TreeImage.java
+++ src/main/java/flyweight/birch_trees/TreeImage.java
@@ -3,8 +3,8 @@ package flyweight.birch_trees;
 import javafx.scene.image.Image;
 
 public class TreeImage implements Flyweight {
-    private Image image;
-    private String description;
+    private final Image image;
+    private final String description;
 
     public TreeImage(String imagePath, String description) {
         this.image = new Image(imagePath);
