f9fb57ca9e097b085e56a7def23e09c2cf70062f
vesavvo <vesavvo@gmail.com>
2024-11-13 13:40:45 +0200

    Rename variables into more generic format

--- src/main/java/bridge/messaging/Main.java
+++ src/main/java/bridge/messaging/Main.java
@@ -2,12 +2,12 @@ package bridge.messaging;
 
 public class Main {
     public static void main(String[] args) {
-        MessageImp email = new EmailImp();
-        Message personalEmailMessage = new PersonalMessage(email);
-        personalEmailMessage.send("Special offer for you! All items -20% only today.");
+        MessageImp imp = new EmailImp();
+        Message message = new PersonalMessage(imp);
+        message.send("Special offer for you! All items -20% only today.");
 
-        MessageImp text = new SMSImp();
-        Message businessTextMessage = new BusinessMessage(text);
-        businessTextMessage.send("Stock value increases by 21% this quarter.");
+        imp = new SMSImp();
+        message = new BusinessMessage(imp);
+        message.send("Stock value increases by 21% this quarter.");
     }
 }
