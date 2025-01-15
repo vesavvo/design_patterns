4ab0dd930efb4418dd9b3c248c0cd387a254180d
vesavvo <vesavvo@gmail.com>
2025-01-15 12:01:48 +0200

    Change Box and Item types to Component in  client code

--- src/main/java/composite/boxes/Main.java
+++ src/main/java/composite/boxes/Main.java
@@ -4,17 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Box box1 = new Box("Large mailing box 1");
-        Box box2 = new Box("Medium component box 2");
-        Box box3 = new Box("Small component box 3");
-        Box box4 = new Box("Small component box 4");
-        Box box5 = new Box("Small component box 5");
+        Component box1 = new Box("Large mailing box 1");
+        Component box2 = new Box("Medium component box 2");
+        Component box3 = new Box("Small component box 3");
+        Component box4 = new Box("Small component box 4");
+        Component box5 = new Box("Small component box 5");
 
-
-        Item item1 = new Item("100 ohm resistor pack");
-        Item item2 = new Item("LED pack");
-        Item item3 = new Item("300 microfarad capacitor pack");
-        Item item4 = new Item("510 microfarad capacitor pack");
+        Component item1 = new Item("100 ohm resistor pack");
+        Component item2 = new Item("LED pack");
+        Component item3 = new Item("300 microfarad capacitor pack");
+        Component item4 = new Item("510 microfarad capacitor pack");
 
         // put Item 1 in box 3
         box3.add(item1);
@@ -35,6 +34,6 @@ public class Main {
         box2.add(box5);
 
         // print the contents of box 1
-        box1.printData();
+        item2.printData();
     }
 }
